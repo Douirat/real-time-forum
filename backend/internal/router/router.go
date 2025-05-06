733fc9b88e494719819ea0c6bd78b2699b186dc9
idmossab <idmossab@gmail.com>
2025-05-06 11:43:29 +0100

    create router

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,16 +1,40 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Router struct {
-	router *http.ServeMux
+	Routes map[string]http.HandlerFunc
 }
 
-func (r *Router) NewRouter(rt *http.ServeMux) *Router {
-	inst := http.NewServeMux()
-	return &Router{router: inst}
+func NewRouter() *Router {
+	return &Router{
+		Routes: make(map[string]http.HandlerFunc),
+	}
 }
 
+func (router *Router) AddRoute(method string, path string, handler http.HandlerFunc) {
+	route := strings.ToLower(method + ":" + path)
+	router.Routes[route] = handler
+}
 
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	origin := r.Header.Get("Origin")
+	fmt.Println("------------> ", origin)
+	w.Header().Set("Access-Control-Allow-Origin", origin)
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	route := strings.ToLower(r.Method + ":" + r.URL.Path)
+	if handler, ok := router.Routes[route]; ok {
+		handler(w, r)
+		return
+	}
+	http.NotFound(w, r)
+}
