e46b97e6eab04e5c220f46ef3975444a282c84f9
midbenke <idmossab@gmail.com>
2025-05-12 13:45:52 +0100

    add show post to frontend

--- frontend/static/post.js
+++ frontend/static/post.js
@@ -31,3 +31,38 @@ export function add_new_post() {
         })
         .catch(errorText => console.log("Error:", errorText));
 }
+
+
+// show posts :
+export function show_posts() {
+    // console.log(">> show_posts called")
+
+    fetch("http://localhost:8080/get_posts")
+        .then(response => {
+            // console.log("Response status:", response.status);
+            return response.json();
+        })
+        .then(data => {
+            // console.log("Fetched data:", data)
+
+            let postsContainer = document.querySelector(".posts");
+            postsContainer.innerHTML = "";
+
+            data.forEach(post => {
+                // console.log("Post:", post)
+
+                let postDiv = document.createElement("div");
+                postDiv.className = "post";
+
+                postDiv.innerHTML = `
+                    <h2>${post.title}</h2>
+                    <p>${post.content}</p>
+                `;
+
+                postsContainer.appendChild(postDiv);
+            });
+        })
+        .catch(error => {
+            console.error("Fetch error:", error);
+        });
+}
