c282a447d5f3e12ebb0947988fe6f71a4f658c58
Mossab Idbenkellal <idmossab@gmail.com>
2025-05-20 14:38:14 +0100

    handler reloead page

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,93 +1,92 @@
-import { registration_form, login_form } from "./components/forms.js"
-import { register_new_user, login_user } from "./users.js"
-import { render_home_page } from "./home.js"
-import { add_new_post } from "./post.js"
-
+import { registration_form, login_form } from "./components/forms.js";
+import { register_new_user, login_user } from "./users.js";
+import { render_home_page } from "./home.js";
+import { add_new_post } from "./post.js";
 
 export function navigateTo(url) {
-    history.pushState(null, null, url)
-    router()
+  history.pushState(null, null, url);
+  router();
 }
 async function router() {
-    let routers = [
-        { path: "/", view: render_home_page },
-        { path: "/register", view: registration_form },
-        { path: "/login", view: login_form }
-    ]
-    console.log(routers);
+  let routers = [
+    { path: "/", view: render_home_page },
+    { path: "/register", view: registration_form },
+    { path: "/login", view: login_form },
+  ];
+  console.log(routers);
 
-    let routes_with_booleans = routers.map(route => {
-        return {
-            route: route,
-            isMatche: location.pathname === route.path,
-        }
-    })
-    console.log(routes_with_booleans);
-    let match = routes_with_booleans.find(selected => selected.isMatche)
-    console.log("selected: ", match);
+  let routes_with_booleans = routers.map((route) => {
+    return {
+      route: route,
+      isMatche: location.pathname === route.path,
+    };
+  });
+  console.log(routes_with_booleans);
+  let match = routes_with_booleans.find((selected) => selected.isMatche);
+  console.log("selected: ", match);
 
-    if (!match) {
-        console.error("Page not found")
-        return
-    }
+  if (!match) {
+    console.error("Page not found");
+    return;
+  }
 
-    if (match.route.view) {
-        match.route.view.apply()
-    }
-    render_events_handlers()
+  if (match.route.view) {
+    match.route.view.apply();
+  }
+  render_events_handlers();
 }
 
 // run my progrqm when the DOM is loaded:
 document.addEventListener("DOMContentLoaded", () => {
-    router()
-    console.log(location.pathname)
-    let a = document.querySelectorAll('a')
-    a.forEach(ele => {
-        ele.addEventListener('click', (e) => {
-            e.preventDefault()
-            navigateTo(ele.href)
-        })
-    })
-})
+  router();
 
+  document.body.addEventListener("click", (e) => {
+    if (e.target.closest('a')) {
+    e.preventDefault();
+    navigateTo(e.target.href);
+    }
+
+  });
+});
 
 // Render events handler:
 function render_events_handlers() {
-    switch (location.pathname) {
-        case "/register":
-            setTimeout(() => {
-                let registration_form = document.getElementById("registration_form")
-                if (registration_form) {
-                    registration_form.addEventListener("submit", (event) => {
-                        event.preventDefault()
-                        register_new_user()
-                    })
-                }
-            }, 0)
-            break
-        case "/login":
-            setTimeout(() => {
-                let login_form = document.getElementById("login_form")
-                if (login_form) {
-                    login_form.addEventListener("submit", (event) => {
-                        event.preventDefault()
-                        login_user()
-                    })
-                }
-            }, 0)
-            break
+  switch (location.pathname) {
+    case "/register":
+      setTimeout(() => {
+        let registration_form = document.getElementById("registration_form");
+        if (registration_form) {
+          registration_form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            register_new_user();
+          });
+        }
+      }, 0);
+      break;
+    case "/login":
+      setTimeout(() => {
+        let login_form = document.getElementById("login_form");
+        if (login_form) {
+          login_form.addEventListener("submit", (event) => {
+            console.log("sqdsqddqsqsd");
+            event.preventDefault();
+            login_user();
+          });
+        }
+      }, 0);
+      break;
 
-        case "/":
-            setTimeout(() => {
-                let posts_form = document.getElementById("posts_form")
-                if (posts_form) {
-                    console.log("hkhkhkh")
-                    posts_form.addEventListener("submit", (event) => {
-                        event.preventDefault()
-                        add_new_post()
-                    })
-                }
-            }, 200)
-            break
-    }
+    case "/":
+      setTimeout(() => {
+        let posts_form = document.getElementById("posts_form");
+        if (posts_form) {
+          console.log("hkhkhkh");
+          posts_form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            add_new_post();
+          });
+        }
+      }, 200);
+      break;
+  }
 }
--- frontend/static/users.js
+++ frontend/static/users.js
@@ -1,91 +1,94 @@
-export async function register_new_user() {
-    let user = {
-        id: 0,
-        first_name: document.getElementById("first_name").value.trim(),
-        last_name: document.getElementById("last_name").value.trim(),
-        age: generate_age(document.getElementById("birth_date").value),
-        email: document.getElementById("email").value.trim(),
-        gender: document.querySelector("input[name='gender']:checked").value,
-        password: document.getElementById("password").value,
-        confirmation: document.getElementById("confirmation").value,
-        nick_name: "-"
-    }
-    console.log(user)
-    if (!isValidPassword(user.password, user.confirmation) || !isValidEmail(user.email)) {
-        alert("invalid email or password")
-        return
-    }
+import { navigateTo } from "./script.js";
 
-    fetch("http://localhost:8080/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
+export async function register_new_user() {
+  let user = {
+    id: 0,
+    first_name: document.getElementById("first_name").value.trim(),
+    last_name: document.getElementById("last_name").value.trim(),
+    age: generate_age(document.getElementById("birth_date").value),
+    email: document.getElementById("email").value.trim(),
+    gender: document.querySelector("input[name='gender']:checked").value,
+    password: document.getElementById("password").value,
+    confirmation: document.getElementById("confirmation").value,
+    nick_name: "-",
+  };
+  console.log(user);
+  if (
+    !isValidPassword(user.password, user.confirmation) ||
+    !isValidEmail(user.email)
+  ) {
+    alert("invalid email or password");
+    return;
+  }
+
+  fetch("http://localhost:8080/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(errorText);
+      }
+      return response.json();
     })
-        .then(async response => {
-            if (!response.ok) {
-                const errorText = await response.text();
-                throw new Error(errorText);
-            }
-            return response.json();
-        })
-        .then(data => console.log("Success:", data))
-        .catch(errorText => console.log("Error:", errorText));
+    .then((data) => navigateTo("/login"))
+    .catch((errorText) => console.log("Error:", errorText));
 }
 
 // Handle user login:
 export async function login_user() {
-    let credentials = {
-        email: document.getElementById("email").value.trim(),
-        password: document.getElementById("password").value,
-    }
-    console.log(credentials);
+  let credentials = {
+    email: document.getElementById("email").value.trim(),
+    password: document.getElementById("password").value,
+  };
+  console.log(credentials);
 
-    if (!isValidEmail(credentials.email) || credentials.email === "" || credentials.password === "") {
-        alert("invalid email or password")
-        return
-    }
-    fetch("http://localhost:8080/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(credentials)
+  if (
+    !isValidEmail(credentials.email) ||
+    credentials.email === "" ||
+    credentials.password === ""
+  ) {
+    alert("invalid email or password");
+    return;
+  }
+  fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(errorText);
+      }
+      navigateTo("/");
+      return response.json();
     })
-        .then(async response => {
-            if (!response.ok) {
-                const errorText = await response.text();
-                throw new Error(errorText);
-            }
-            let a = document.createElement("a")
-            a.href = "/"
-            a.click()
-            return response.json();
-        })
-        .then(data => () => {
-            // console.log(data)
-        })
-        .catch(errorText => console.log("Error:", errorText));
+    .then((data) => navigateTo("/register"))
+    .catch((errorText) => console.log("Error:", errorText));
 }
-
-
 
 // Generate age:
 let generate_age = (born) => {
-    let birth_date = new Date(born)
-    let current_date = new Date()
-    let age = current_date.getFullYear() - birth_date.getFullYear()
-    return age
-}
+  let birth_date = new Date(born);
+  let current_date = new Date();
+  let age = current_date.getFullYear() - birth_date.getFullYear();
+  return age;
+};
 
 // Is valid password:
 let isValidPassword = (password, confirmation) => {
-    return password === confirmation && password.length >= 8
-}
+  return password === confirmation && password.length >= 8;
+};
 
 // is valid email:
 function isValidEmail(email) {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
 }
