f829c73694b2a6c207ed6790d119d5dd2c314e81
midbenke <idmossab@gmail.com>
2025-06-19 15:18:57 +0100

    edit submit from stiwtch to map

--- front/app.js
+++ front/app.js
@@ -2,31 +2,31 @@ import { navigateTo, router } from "./router/router.js";
 import { register_new_user, login_user } from "./views/users.js";
 import { add_new_post } from "./views/post.js";
 
+
+const formHandlers = new Map([
+  ["registration_form", register_new_user],
+  ["login_form", login_user],
+  ["posts_form", add_new_post],
+]);
+
 document.addEventListener("DOMContentLoaded", () => {
-    router();
-    
-    document.body.addEventListener("click", (e) => {
-        if (e.target.closest("a")) {
-            e.preventDefault();
-            navigateTo(e.target.href);
-        }
-    });
-    
-    document.body.addEventListener("submit", (e) => {
-        e.preventDefault();
-        
-        switch (e.target.id) {
-            case "registration_form":
-                register_new_user();
-                break;
-            case "login_form":
-                login_user();
-                break;
-            case "posts_form":
-                add_new_post();
-                break;
-            default:
-                console.warn("Unknown form submitted:", e.target.id);
-        }
-    });
+  router();
+
+  document.body.addEventListener("click", (e) => {
+    if (e.target.closest("a")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+
+  document.body.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const handler = formHandlers.get(e.target.id);
+    if (handler) {
+      handler();
+    } else {
+      console.warn("Unknown form submitted:", e.target.id);
+    }
+  });
 });
