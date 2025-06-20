fabf29942f0ba859d21b94299ff6b5bea49f9f4a
midbenke <idmossab@gmail.com>
2025-06-20 19:34:24 +0100

    added page error to fetch comment

--- front/views/comments/fetchComments.js
+++ front/views/comments/fetchComments.js
@@ -1,17 +1,31 @@
 import { formatDate } from '../../utils/comment_validators.js';
+import { render_error_page } from "../error.js";
+import { getErrorMessage } from "../../utils/error_validators.js";
 
 export function show_comments_for_post(postId) {
     fetch(`http://localhost:8080/get_comments?id=${postId}`)
-        .then(res => {
-            if (!res.ok) throw new Error("Failed to fetch comments");
+        .then(async res => {
+            if (!res.ok) {
+                const errorText = await res.text();
+                const error = new Error(errorText);
+                error.status = res.status;
+                throw error;
+            }
             return res.json();
         })
         .then(comments => {
             const container = document.getElementById(`comments-container-${postId}`);
             if (!container) return;
-            container.innerHTML = comments.length === 0? `<p>No comments yet</p>`: renderCommentsList(comments.reverse());
+            container.innerHTML = comments.length === 0 ? `<p>No comments yet</p>` : renderCommentsList(comments.reverse());
         })
-        .catch(err => console.error("Error fetching comments:", err));
+        .catch(err => {
+            console.error("Error fetching comments:", err);
+            if (err.status) {
+                render_error_page(err.status, getErrorMessage(err.status));
+            } else {
+                render_error_page(500, "Failed to Error fetching comments due to an unknown error");
+            }
+        });
 }
 
 function renderCommentsList(comments) {
