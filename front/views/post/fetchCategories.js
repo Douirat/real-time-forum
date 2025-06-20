633cca15ce870de5e5e969f565f9bf3e905e7968
midbenke <idmossab@gmail.com>
2025-06-20 19:47:21 +0100

    added page error to fetch category

--- front/views/post/fetchCategories.js
+++ front/views/post/fetchCategories.js
@@ -1,12 +1,21 @@
 // fetchCategories.js
 export function fetch_categories() {
     return fetch("http://localhost:8080/get_categories")
-        .then(response => {
-            if (!response.ok) throw new Error("Failed to fetch categories");
-            return response.json();
+        .then(async response => {
+            if (!res.ok) {
+                const errorText = await res.text();
+                const error = new Error(errorText);
+                error.status = res.status;
+                throw error;
+            }
+            return res.json();
         })
         .catch(error => {
             console.error("Error fetching categories:", error);
-            return [];
+            if (err.status) {
+                render_error_page(err.status, getErrorMessage(err.status));
+            } else {
+                render_error_page(500, "Failed fetching categories due to an unknown error");
+            }
         });
 }
