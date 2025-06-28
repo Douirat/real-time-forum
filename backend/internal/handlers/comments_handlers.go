8b21f42fb459bd5fa3ecfd15e838ee26bb92fd36
Mossab Idbenkellal <idmossab@gmail.com>
2025-06-28 13:36:11 +0100

    appley parselimit ofsset in handfler

--- backend/internal/handlers/comments_handlers.go
+++ backend/internal/handlers/comments_handlers.go
@@ -55,23 +55,14 @@ func (comHand *CommentsHandler) ShowCommentsHandler(w http.ResponseWriter, r *ht
 	}
 	queryParam := r.URL.Query()
 	query := queryParam.Get("id")
-	offsetStr := r.URL.Query().Get("offset")
-	limitStr := r.URL.Query().Get("limit")
 
 	id, err := strconv.Atoi(query)
 	if err != nil {
 		utils.ResponseJSON(w, http.StatusBadRequest, map[string]any{"message": "query not allowed"})
 		return
 	}
-	// Default to 0 and 10 if not provided
-	offset, err := strconv.Atoi(offsetStr)
-	if err != nil {
-		offset = 0
-	}
-	limit, err := strconv.Atoi(limitStr)
-	if err != nil || limit <= 0 {
-		limit = 10
-	}
+	offset, limit := utils.ParseLimitOffset(r)
+
 	comments, err := comHand.ComSer.ShowCommentsservice(id, offset, limit)
 	if err != nil {
 		utils.ResponseJSON(w, http.StatusInternalServerError, map[string]any{"message": "error getComment"})
