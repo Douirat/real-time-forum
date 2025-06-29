c8a05a12735e5bbf312784f1a39ab55c69f2586d
Mossab Idbenkellal <idmossab@gmail.com>
2025-06-29 11:36:46 +0100

    edit component header

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,7 +1,3 @@
-import { navigateTo } from "../router/router.js";
-import { render_error_page } from "../views/error.js";
-import { getErrorMessage } from "../utils/error_validators.js";
-
 export function header() {
     return /*html*/`
         <header class="header">
@@ -11,38 +7,4 @@ export function header() {
             </nav>
         </header>
     `;
-}
-
-export function logout() {
-    let btn = document.querySelector('.logout');
-    if (btn) {
-        btn.addEventListener('click', () => {
-            fetch("http://localhost:8080/logout", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            })
-                .then(async response => {
-                    if (!response.ok) {
-                        const errorText = await response.text();
-                        const error = new Error(errorText);
-                        error.status = response.status;
-                        throw error;
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    navigateTo("/login");
-                })
-                .catch(error => {
-                    console.error("Logout error:", error);
-                     if (error.status && error.status >= 500) {
-                        render_error_page(error.status, getErrorMessage(error.status));
-                    } else {
-                        navigateTo("/login");
-                    }
-                });
-        });
-    }
 }
