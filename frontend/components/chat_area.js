600bc63a61a0603a3267fc46396ee6dd3d05f308
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-06-30 12:39:07 +0100

    a simple design for real time forum

--- frontend/components/chat_area.js
+++ frontend/components/chat_area.js
@@ -6,17 +6,15 @@ export function render_char_area() {
           <h3>Chat</h3>
           <button id="cancel_chat"><img src="/assets/imgs/delete-button.png" alt="x"></button>
         </div>
-
         <div id="messages-container" class="messages-container">
           <!-- Messages will appear here -->
         </div>
-
         <!-- Typing indicator placeholder -->
         <div id="typing-indicator" class="typing-indicator"></div>
 
         <div class="message-input-container">
           <input type="text" id="message-input" placeholder="Type a message..." />
-          <button id="send-button">Send</button>
+          <button id="send-button"><img src="/assets/imgs/send-message.png" alt="send"></button>
         </div>
       </div>
     </div>
